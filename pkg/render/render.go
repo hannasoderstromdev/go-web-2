8368a05a5fe88724e65b4c1768952d7b136246fd
Hanna Söderström <4538260+hannasoderstromdev@users.noreply.github.com>
2023-09-26 16:44:04 +0200

    refactor: use base layout and add cache

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -2,13 +2,14 @@ package render
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"text/template"
 )
 
 // RenderTemplate parses a template file and renders it as HTML
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+func RenderTemplateTest(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template:", err)
@@ -16,4 +17,50 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	
 	
 	}
+}
+
+var tc = make(map[string]*template.Template)
+
+func RenderTemplate(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	// is template in cache?
+	_, inMap := tc[t]
+	if !inMap {
+		// create template
+		log.Println("creating template and adding to cache")
+		err = createTemplateCache(t)
+		if err != nil {
+			log.Println(err)
+		}
+	} else {
+		// exists in cache
+		log.Println("using cached template")
+	}
+
+	tmpl = tc[t]
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t),
+		"./templates/base.layout.tmpl",
+	}
+
+	// parse
+	tmpl, err := template.ParseFiles(templates...)
+
+	if err != nil {
+		return err
+	}
+
+	tc[t] = tmpl
+
+	return nil
 }
