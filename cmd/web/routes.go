dbba3cf445f8227d48a81375b0e7dda28ef45833
Hanna Söderström <4538260+hannasoderstromdev@users.noreply.github.com>
2023-09-28 09:47:27 +0200

    refactor: replace pat router with chi router

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,16 +3,21 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
 	"github.com/hannasoderstromdev/go-web-2/pkg/config"
 	"github.com/hannasoderstromdev/go-web-2/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	// mux := pat.New()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
