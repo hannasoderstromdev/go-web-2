3e5b3116757a6677528afbb2417eb3ebdd70e43a
Hanna Söderström <4538260+hannasoderstromdev@users.noreply.github.com>
2023-09-29 12:58:41 +0200

    feature: add template middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,17 +4,17 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/hannasoderstromdev/go-web-2/pkg/config"
 	"github.com/hannasoderstromdev/go-web-2/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	// mux := pat.New()
-
-	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-
 	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer) // Prevents panic
+	mux.Use(WriteToConsole) // Writes to console
+
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
